287ddfdfe194c427ea51ea513ec85e02eb88cabf
ashiq-rahman07 <web3.0.ashiq@gmail.com>
2024-11-08 21:14:02 +0600

    p1 done

---
+++ src/problem-1.ts
@@ -0,0 +1,35 @@
+{
+
+
+// Problem -1 : summ of array 
+
+
+const sumArray=(numbers:number[]):number=>{
+    let sum:number = 0;
+
+    for(const num of numbers){
+        sum += num;
+    }
+
+    return sum;
+}
+
+
+
+// Sample Input:
+console.log(sumArray([1, 2, 3, 4, 5]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
